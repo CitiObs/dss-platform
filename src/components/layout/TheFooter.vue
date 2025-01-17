5d5505f34c3d84076b4cc2f52b7dd3a700c3edd3
Giorgos Akritidis <gakritidis@draxis.gr>
2025-01-17 16:37:39 +0200

    refactor: Update app footer
    issue: CIT-9

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { STYLES_CONFIG } from "@/common/stylesConfig.js";
+
+const euFundedImgHeight = `${STYLES_CONFIG.footer_height / 2}px`;
 </script>
 
 <template>
@@ -10,20 +12,45 @@ import { STYLES_CONFIG } from "@/common/stylesConfig.js";
     >
         <v-footer
             :height="STYLES_CONFIG.footer_height"
+            class="footer px-5"
         >
-            <v-row class="align-center">
-                <v-col><p>Copyright Message</p></v-col>
+            <div class="footer__content flex-grow-1 d-flex align-center justify-space-around gc-4 mx-auto">
+                <p class="footer__copyright gi-text-base">
+                    © 2024, All rights reserved.
+                </p>
 
-                <v-col><p>Horizon 2020</p></v-col>
+                <img
+                    class="footer__eu-funded-img"
+                    src="../../assets/images/eu-funded.png"
+                    alt="Co-funded by the European Union"
+                />
 
-                <v-col><div>Eu Image</div></v-col>
-
-                <v-col cols="5">
-                    <p style="font-size: .8rem;">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quam quos repellat impedit rem, quae non quibusdam iure natus aperiam aspernatur numquam beatae! Quibusdam, dicta consectetur autem incidunt blanditiis ex.
-                    </p>
-                </v-col>
-            </v-row>
+                <p class="footer__eu-funded-desc gi-text-sm">
+                    This project has received funding from the European Union’s Horizon Europe programme for research and innovation under grant agreement No. 101121135.
+                </p>
+            </div>
         </v-footer>
     </v-container>
 </template>
+
+<style scoped lang="scss">
+.footer {
+    &__content {
+        max-width: 85%;
+    }
+
+    &__copyright {
+      color: var(--gi-black-400);
+    }
+
+    &__eu-funded-img {
+        max-height: v-bind(euFundedImgHeight);
+    }
+
+    &__eu-funded-desc {
+        flex-basis: 40%;
+        max-width: 40rem;
+        color: var(--gi-grey-800);
+    }
+}
+</style>
